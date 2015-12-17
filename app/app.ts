d0acbf7d3aad54c585f58d4e1d260300a4ce2a70
Michal Raczkowski <michal.raczkowski@zalando.de>
2015-12-17 15:29:03 +0100

    fixed directive usage - camelCase

--- app/app.ts
+++ app/app.ts
@@ -6,12 +6,13 @@ import {MyInput} from "./my-input";
 @Component({
     selector: "my-app",
     directives: [MyInput, FORM_DIRECTIVES],
-    template: `<div>MY APP</div>
-               <my-input></my-input>
+    template:   `
+                <div>MY APP</div>
+                <my-input></my-input>
 
-               <h1>{{text}}</h1>
-               <input type="text" [ng-model]="text" (ng-model-change)="text = $event" />
-               `
+                <h1>{{text}}</h1>
+                <input type="text" [(ngModel)]="text" />
+                `
 })
 export class MyApp {}
 
--- app/my-input.ts
+++ app/my-input.ts
@@ -2,8 +2,8 @@ import {Component, Output, EventEmitter} from "angular2/core";
 
 @Component({
     selector: "my-input",
-    template: `<input #my_input [type]="myType" [value]="myValue" />
-               <button (click)="onClick(my_input)">X</button>`
+    template: `<input #myInput [type]="myType" [value]="myValue" />
+               <button (click)="onClick(myInput)">X</button>`
 })
 export class MyInput {
     myType;
